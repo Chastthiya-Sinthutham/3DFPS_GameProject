da8baf9d567bac67be9bb61b471c9695e23f685f
Panuwat47 <panuwat.no@kkumail.com>
2025-10-08 22:05:30 +0700

    tutorial

--- Starter-Kit-FPS-cc46ba7b2b14275916429df7fdf7692f431902f2/objects/justportal.gd
+++ Starter-Kit-FPS-cc46ba7b2b14275916429df7fdf7692f431902f2/objects/justportal.gd
@@ -4,4 +4,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	call_deferred("_change_scene")
 
 func _change_scene():
-	get_tree().change_scene_to_file("res://scenes/victory.tscn")
+	get_tree().change_scene_to_file("res://scenes/lv_1.tscn")
